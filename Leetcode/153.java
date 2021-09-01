0d50653fbab821291968a662f13f479e43a10a6d
RainerYuan <36510771+RainerYuan@users.noreply.github.com>
2021-08-31 23:51:15 -0700

    152

---
+++ Leetcode/153.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMin(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+    private int helper(int[] nums, int s, int e){
+        if(s+1 >= e){
+            int min = (nums[s]>nums[e])? nums[e]:nums[s];
+            return min;
+        }
+        int mid = (s+e)/2;
+        int leftmin = helper(nums,s,mid);
+        int rightmin = helper(nums,mid+1,e);
+        int finalmin = (leftmin>rightmin)? rightmin:leftmin;
+        return finalmin;
+    }
+}
